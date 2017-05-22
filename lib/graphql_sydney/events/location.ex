ae385cc882420a26263ec5ba6056fa0dca81bdf6
Josh Price <josh@alembic.com.au>
2017-05-22 12:20:15 +1000

    Fix location schema

--- lib/graphql_sydney/events/location.ex
+++ lib/graphql_sydney/events/location.ex
@@ -5,6 +5,11 @@ defmodule GraphqlSydney.Events.Location do
 
   schema "location" do
     field :name, :string
+    field :street_address, :string
+    field :suburb, :string
+    field :state, :string
+    field :country, :string
+    field :postcode, :string
     field :map_url, :string
 
     has_many :meetups, Meetup
