5ae35a0ea982bbb22e745454ffc7bfc70735a300
Josh Price <josh@alembic.com.au>
2017-11-26 20:00:30 +1100

    Add December 2017 meetup

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,6 +18,18 @@ Repo.delete_all(Person)
 Repo.delete_all(Meetup)
 Repo.delete_all(Location)
 
+domain = Repo.insert!(
+  %Location{
+    name: "Domain",
+    street_address: "55 Pyrmont St",
+    suburb: "Pyrmont",
+    state: "NSW",
+    country: "Australia",
+    postcode: "2009",
+    map_url: "https://www.google.com/maps/place/55+Pyrmont+St,+Pyrmont+NSW+2009/@-33.86814,151.1940918,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12ae36929b8279:0x5f882e03d383cc12!8m2!3d-33.86814!4d151.1940918"
+  }, on_conflict: :replace_all, conflict_target: :name
+)
+
 we_work = Repo.insert!(
   %Location{
     name: "WeWork",
@@ -35,7 +47,7 @@ josh_price = Repo.insert!(
     name: "Josh Price",
     avatar_url: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/4/5/c/e/member_12197870.jpeg",
     meetup_url: "https://www.meetup.com/en-AU/sydney-ex/members/8425371/",
-    website_url: "http://alembic.com.au",
+    website_url: "https://alembic.com.au",
     twitter: "@joshprice"
   }, on_conflict: :replace_all, conflict_target: :name
 )
@@ -75,12 +87,92 @@ james_sadler = Repo.insert!(
     name: "James Sadler",
     avatar_url: "https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/2/8/f/b/member_258850491.jpeg",
     meetup_url: "https://www.meetup.com/en-AU/sydney-ex/members/202221787/",
-    website_url: "http://alembic.com.au",
+    website_url: "https://alembic.com.au",
     twitter: "@freshtonic"
   }, on_conflict: :replace_all, conflict_target: :name
 )
 
+dave_parry = Repo.insert!(
+  %Person{
+    name: "Dave Parry",
+    avatar_url: "https://secure.meetupstatic.com/photos/member/d/3/4/c/member_81234092.jpeg",
+    meetup_url: "https://www.meetup.com/sydney-ex/members/12433325/",
+    website_url: "http://suranyami.com/",
+    twitter: "@suranyami"
+  }, on_conflict: :replace_all, conflict_target: :name
+)
+
+sputnik_antolovich = Repo.insert!(
+  %Person{
+    name: "Sputnik Antolovich",
+    avatar_url: "https://secure.meetupstatic.com/photos/member/3/4/6/2/highres_269533410.jpeg",
+    meetup_url: "https://www.meetup.com/GraphQL-Sydney/members/200796744/",
+    website_url: "https://trioxis.com/",
+    twitter: "https://www.facebook.com/sputnik.antolovich"
+  }, conflict_target: :replace_all, on_conflict: :replace_all
+)
+
+
 raw_meetups = [
+  %Meetup{
+    title: "Realtime and Serverless APIs",
+    subtitle: "",
+    slug: "realtime-and-serverless-apis-with-graphql",
+    date: ~D[2017-12-14],
+    location: domain,
+    description: """
+    We're bringing together everyone who is as excited about the potential of GraphQL as we are.
+
+    We've got an inspiring lineup of speakers to help introduce you to GraphQL and show you how it can help you build better APIs faster than ever.
+
+    We'll be trialling an incredible new venue too, thanks to [Domain](https://www.domain.com.au).
+
+    Some light food and drinks will be available thanks to [Alembic](https://alembic.com.au).
+
+    [Follow us on Twitter](https://twitter.com/graphqlsydney) for updates.
+
+    Can't wait to see you there!
+
+    _Josh, James and Suzie_
+    """,
+    url: "https://www.meetup.com/GraphQL-Sydney/events/245093762/",
+    talks: [
+      %Talk{
+        title: "GraphQL 101",
+        slug: "graphql-101",
+        description: """
+        A short intro to practical GraphQL basics to get you up to speed quickly.
+        """,
+        presenter: dave_parry
+      },
+      %Talk{
+        title: "Serverless GraphQL APIs",
+        slug: "serverless-graphql-apis",
+        description: """
+        An exploration of how Serverless (or Functions as a Service) are being used to back GraphQL APIs.
+        """,
+        presenter: sputnik_antolovich
+      },
+      %Talk{
+        title: "Real-time APIs with GraphQL Subscriptions",
+        slug: "real-time-apis-with-graphql-subscriptions",
+        description: """
+        One of the features that gives GraphQL a major advantage over REST is it's ability to take
+        advantage of persistent connections and provide real-time updates to connected clients.
+
+        Now that subscriptions has finally been merged back into the GraphQL spec, incorporated
+        into the major server implementations and HTTP/2 usage in on the rise, it's time
+        to take full advantage of this feature.
+
+        James will demonstrate how you can use this in your own applications. He'll present
+        a demonstration of [Apollo Client 2.0](https://dev-blog.apollodata.com/apollo-client-2-0-5c8d0affcec7)
+        connecting to an [Absinthe](http://absinthe-graphql.org/) backend using the
+        power of [Phoenix](http://phoenixframework.org/) [Channels](https://hexdocs.pm/phoenix/channels.html).
+        """,
+        presenter: james_sadler
+      }
+    ]
+  },
   %Meetup{
     title: "Get Ready For The Realtime Web",
     subtitle: "First GraphQL Sydney Meetup",
@@ -142,20 +234,6 @@ raw_meetups = [
         Chris will take you on their journey to stronger typing and the benefits.
         """,
         presenter: chris_giffard
-      },
-      %Talk{
-        title: "Real-time APIs with GraphQL Subscriptions",
-        slug: "real-time-apis-with-graphql-subscriptions",
-        description: """
-        One of the features that gives GraphQL a major advantage over REST is it's ability to take
-        advantage of persistent connections and provide real-time updates to connected clients.
-
-        Now that subscriptions has finally been merged back into the GraphQL spec,
-        and HTTP/2 usage in on the rise, it's time to take full advantage of this feature.
-
-        James will demonstrate how you can use this in your own applications.
-        """,
-        presenter: james_sadler
       }
     ]
   }
