085b2695252ffe00c00d5df2496772f0ad47dc91
James Sadler <james@alembic.com.au>
2018-03-06 16:34:14 +1100

    Added code of conduct page

--- lib/graphql_sydney/web/controllers/page_controller.ex
+++ lib/graphql_sydney/web/controllers/page_controller.ex
@@ -4,41 +4,62 @@ defmodule GraphqlSydney.Web.PageController do
   alias GraphqlSydney.Events
 
   def index(conn, _params) do
-    next_meetup = Events.next_meetup
+    next_meetup = Events.next_meetup()
 
-    render conn, "index.html",
+    render(
+      conn,
+      "index.html",
       meetup: next_meetup,
       page_name: "home",
       title: next_meetup.title
+    )
   end
 
   def about(conn, _params) do
-    render conn, "about.html",
+    render(
+      conn,
+      "about.html",
       page_name: "about",
       title: "About"
+    )
+  end
+
+  def code_of_conduct(conn, _params) do
+    render(
+      conn,
+      "code_of_conduct.html",
+      page_name: "code_of_conduct",
+      title: "Code of Conduct"
+    )
   end
 
   def meetups(conn, _params) do
-    next_meetup = Events.next_meetup
+    next_meetup = Events.next_meetup()
 
-    render conn, "meetups.html",
+    render(
+      conn,
+      "meetups.html",
       next_meetup: next_meetup,
-      past_meetups: Events.past_meetups,
-      all_meetups: Events.all_meetups,
+      past_meetups: Events.past_meetups(),
+      all_meetups: Events.all_meetups(),
       page_name: "meetups",
       title: next_meetup.title
+    )
   end
 
   def meetup(conn, %{"slug" => slug}) do
     meetup = Events.meetup_for_slug(slug)
 
     if meetup do
-      render conn, "meetup.html",
+      render(
+        conn,
+        "meetup.html",
         meetup: meetup,
         page_name: "meetup",
         title: meetup.title
+      )
     else
-      redirect conn, to: page_path(conn, :meetups)
+      redirect(conn, to: page_path(conn, :meetups))
     end
   end
 end
--- lib/graphql_sydney/web/router.ex
+++ lib/graphql_sydney/web/router.ex
@@ -2,39 +2,40 @@ defmodule GraphqlSydney.Web.Router do
   use GraphqlSydney.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug Plug.Parsers,
+    plug(
+      Plug.Parsers,
       parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
       json_decoder: Poison
+    )
   end
 
   scope "/", GraphqlSydney.Web do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    get "/about", PageController, :about
-    get "/meetups", PageController, :meetups
-    get "/meetup/:slug", PageController, :meetup, param: "slug"
+    get("/", PageController, :index)
+    get("/about", PageController, :about)
+    get("/code_of_conduct", PageController, :code_of_conduct)
+    get("/meetups", PageController, :meetups)
+    get("/meetup/:slug", PageController, :meetup, param: "slug")
   end
 
   scope "/graphql" do
-    pipe_through :api
+    pipe_through(:api)
 
-    forward "/", Absinthe.Plug,
-      schema: GraphqlSydney.GraphQL.Schema
+    forward("/", Absinthe.Plug, schema: GraphqlSydney.GraphQL.Schema)
   end
 
   scope "/graphiql" do
-    pipe_through :api
+    pipe_through(:api)
 
-    forward "/", Absinthe.Plug.GraphiQL,
-      schema: GraphqlSydney.GraphQL.Schema
+    forward("/", Absinthe.Plug.GraphiQL, schema: GraphqlSydney.GraphQL.Schema)
   end
 end
